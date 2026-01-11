98478d467e5a75b8195e4599d94c37c7ff1f68d4
ming <mingjia.su@szdoortech.com>
2026-01-11 17:17:57 +0800

    同上一次更新 遗漏

--- boot_update_test/Mod/External_Interfaces/api_flash_port.h
+++ boot_update_test/Mod/External_Interfaces/api_flash_port.h
@@ -5,15 +5,29 @@
 extern "C" {
 #endif
 
-#include "main.h"
+#include "mid_eeprom.h"
 
-#define HAL_FLASH_BASE_ADDR      (uint32_t)(FLASH_BASE)		 			//FLASH起始地址---需按照实际定义
-#define HAL_FLASH_END_ADDR       (uint32_t)(FLASH_BANK1_END) 			//FLASH结束地址---需按照实际定义
-#define HAL_FLASH_PAGE_SIZE	     (1 * 2048)								//页大小---需按照实际定义
-#define HAL_MIN_WRITE_baye        4 									//最小写入字节数---需与bsp_flash_write接口的写入量对应
+#define HAL_FLASH_BASE_ADDR      (uint32_t)(0x00000000U)		 		//FLASH起始地址---需按照实际定义
+#define HAL_FLASH_END_ADDR       (uint32_t)(0x00020000U) 			    //FLASH结束地址---需按照实际定义
+#define HAL_FLASH_PAGE_SIZE	     (1 * 64)								//页大小---需按照实际定义
+#define HAL_MIN_WRITE_baye        64 									//最小写入字节数---需与bsp_flash_write接口的写入量对应
+#define HAL_BAND_WIDTH            4                                     //带宽字节数
 
 #define HAL_FLASH_SIZE      (HAL_FLASH_END_ADDR - HAL_FLASH_BASE_ADDR)	//FLASH总容量
 #define HAL_FLASH_PAGE_NUMBER (HAL_FLASH_SIZE/HAL_FLASH_PAGE_SIZE) 		//页数
+
+#if (HAL_BAND_WIDTH == 1)
+typedef uint8_t  FlashBandwidthType_t;  // 1字节带宽 → 8位类型
+#elif (HAL_BAND_WIDTH == 2)
+typedef uint16_t FlashBandwidthType_t;  // 2字节带宽 → 16位类型
+#elif (HAL_BAND_WIDTH == 4)
+typedef uint32_t FlashBandwidthType_t;  // 4字节带宽 → 32位类型
+#elif (HAL_BAND_WIDTH == 8)
+typedef uint64_t FlashBandwidthType_t;  // 8字节带宽 → 64位类型
+#else
+#error "HAL_BAND_WIDTH仅支持1/2/4/8字节"
+#endif
+
 
 #ifndef HAL_FLASH_BASE_ADDR
     #error "请在当前文件或用户配置文件中定义 HAL_FLASH_BASE_ADDR（FLASH起始地址）"
@@ -51,7 +65,7 @@ typedef enum
 
 void api_flash_lock(void);
 void api_flash_unlock(void);
-RUN_StatusTypeDef api_flash_write(uint32_t addr, uint64_t data);
+RUN_StatusTypeDef api_flash_write(uint32_t addr, void* data[]);
 RUN_StatusTypeDef api_flash_page_erase(uint32_t addr);
 void api_irq_enable(void);
 void api_irq_disable(void);
